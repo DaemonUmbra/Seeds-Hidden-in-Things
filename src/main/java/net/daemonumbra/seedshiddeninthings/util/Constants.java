cebf433b36557543d9a967c74f360a38ee50d59d
DaemonUmbra <daemon.umbra@gmail.com>
2018-08-19 00:12:16 -0400

    Fixed Derped Versioning, added VSCode Workspace
    Not much has changed functionally, but I've changed my versioning to me more in line with [Forge's recommended mod versioning](https://mcforge.readthedocs.io/en/latest/conventions/versioning/).

--- src/main/java/net/daemonumbra/seedshiddeninthings/util/Constants.java
+++ src/main/java/net/daemonumbra/seedshiddeninthings/util/Constants.java
@@ -1,7 +1,5 @@
 package net.daemonumbra.seedshiddeninthings.util;
 
-import java.util.Random;
-
 /**
  * Stores global constants for SHiT
  */
@@ -9,10 +7,10 @@ public class Constants {
     //@Mod Constants
     public static final String MOD_ID = "seedshiddeninthings";
     public static final String MOD_NAME = "Seeds Hidden in Things";
-    public static final String VERSION = "0.1.0.0";
+    public static final String VERSION = "@VERSION@";
     public static final String UPDATE_URL = "https://raw.githubusercontent.com/DaemonUmbra/Version/master/SHiT.json";
     public static final String FINGERPRINT = "b0f42f895fac10ce8916e8ce24cb19ecc8d5734b";
-    public static final String MC_VERSION = "[1.12.2,1.13)";
+    public static final String MC_VERSION = "[1.12.2]";
 
     //@SidedProxy Constants
     public static final String SERVER_PROXY = "net.daemonumbra.seedshiddeninthings.proxy.ServerProxy";
